dda4cb89cf47db59a44a6212173cabe306f97c93
Eugene van zyl <eugene.vanzyl@mintgroup.net>
2020-10-17 08:41:09 +0200

    Added temp assets

---
+++ src/components/ItemCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-card outlined>
+    <v-img class="white--text align-end" height="512px" :src="fullSrc">
+      <v-card-title style="text-shadow: 0 0 16px black;">Top 10 Australian beaches</v-card-title>
+    </v-img>
+
+    <v-card-subtitle class="pb-0">Number 10</v-card-subtitle>
+
+    <v-card-text class="text--primary">
+      <div>Whitehaven Beach</div>
+      <div>Whitsunday Island, Whitsunday Islands</div>
+      <div>{{ fullSrc }}</div>
+    </v-card-text>
+
+    <v-card-actions>
+      <v-btn color="orange" text>Share</v-btn>
+      <v-btn color="orange" text>Explore</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+    props: {
+        data: {
+            type: Object,
+            required: true
+        }
+    },
+    computed: {
+        fullSrc() { return `../assets/${this.data.src}` }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/NewArrivals.vue
+++ src/views/NewArrivals.vue
@@ -1,17 +1,20 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-row class="pa-5">
+    <v-col v-for="item in items" :key="item.src" cols="12" md="4">
+      <item-card :data="item"></item-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import ItemCard from '@/components/ItemCard.vue';
 export default {
-  components: {
-    HelloWorld
-  }
+  components: { ItemCard },
+  data: () => ({
+    items: [
+      { src: 'AF3.jpg' }, { src: 'african-fashion-models.jpg' }, { src: 'African-FAshion-week-NC-_TK-eye-Photography_AFWNC_african-fashion-Afican-style_ankara-fashion_african-fashion-week_african-fashion-designers_mnl-design_kizonga16.jpg'}, { src: 'afriken-dress-e-2.jpg'}, { src: 'asakeoge-africa-fashion-week-london-2015-14.jpg'}, { src: 'Blue-Dress-with-Matching-Headscarf.jpg'}, { src: 'Day+Arise+African+Fashion+Collective+Runway+DMRyOkEPQDbx.jpg'}, { src: 'MG_4029.jpeg'}, { src: 'Regalia.jpg'}, { src: 'ss-2014_mbfwb-africa-day-nadir-tati-1.jpg'}
+    ]
+  })
 }
 </script>
