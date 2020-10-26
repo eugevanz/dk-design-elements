a792be8cd4378028348b2df9a4711220777b6a1c
Eugene van zyl <eugene.vanzyl@mintgroup.net>
2020-10-26 10:40:19 +0200

    Starting with Firebase

--- src/main.js
+++ src/main.js
@@ -10,3 +10,14 @@ new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+
+// var firebaseConfig = {
+//   apiKey: "AIzaSyAMazXV0swD5GSoaYbD33l3e8PzxJCxfZ8",
+//   authDomain: "dkdelementsfirestore.firebaseapp.com",
+//   databaseURL: "https://dkdelementsfirestore.firebaseio.com",
+//   projectId: "dkdelementsfirestore",
+//   storageBucket: "dkdelementsfirestore.appspot.com",
+//   messagingSenderId: "437308729251",
+//   appId: "1:437308729251:web:24db781dca0658d61f5c16",
+//   measurementId: "G-YW3YKQ0BSM"
+// };
